81f1a5aab0dff282a7a55c6cf6c1d56f96d29bc3
cslim <chansoon0623@gmail.com>
2025-04-19 12:00:46 +0900

    add to_layout_stride

--- ctmd/core/ctmd_core_convert.hpp
+++ ctmd/core/ctmd_core_convert.hpp
@@ -19,12 +19,31 @@ template <md_c in_t>
 }
 
 template <mdspan_c in_t>
-[[nodiscard]] inline constexpr auto to_const(const in_t &in = in_t{}) noexcept {
+[[nodiscard]] inline constexpr auto to_const(const in_t &in) noexcept {
     using const_element_t = const typename in_t::element_type;
     return mdspan<const_element_t, typename in_t::extents_type,
                   typename in_t::layout_type,
                   std::experimental::default_accessor<const_element_t>>(
         in.data_handle(), in.mapping(), {});
+}
+
+template <mdspan_c in_t>
+[[nodiscard]] inline constexpr auto to_layout_stride(const in_t &in) noexcept {
+    if constexpr (std::is_same_v<typename in_t::layout_type, layout_stride>) {
+        return in;
+
+    } else {
+        const auto new_strides =
+            [&in]<size_t... Is>(std::index_sequence<Is...>) {
+                return std::array<typename in_t::size_type, in_t::rank()>{
+                    in.stride(Is)...};
+            }(std::make_index_sequence<in_t::rank()>{});
+
+        return mdspan<typename in_t::element_type, typename in_t::extents_type,
+                      layout_stride, typename in_t::accessor_type>{
+            in.data_handle(),
+            layout_stride::mapping{in.extents(), new_strides}};
+    }
 }
 
 } // namespace core
