8e9f5ffcf6d0e754ae7ae41d980d273d8fd4b360
cslim <chansoon0623@gmail.com>
2025-05-20 00:40:17 +0900

    developed

---
+++ ctmd/ctmd_atan2.hpp
@@ -0,0 +1,60 @@
+#pragma once
+
+#include <cmath>
+
+#include "core/ctmd_core.hpp"
+
+namespace ctmd {
+namespace detail {
+
+template <mdspan_c in1_t, mdspan_c in2_t, mdspan_c out_t>
+    requires(in1_t::rank() == 0 && in2_t::rank() == 0 && out_t::rank() == 0)
+inline constexpr void atan2_impl(const in1_t &in1, const in2_t &in2,
+                                 const out_t &out) noexcept {
+    using element_t = std::remove_cvref_t<decltype(out())>;
+
+    out() = std::atan2(static_cast<element_t>(in1()),
+                       static_cast<element_t>(in2()));
+}
+
+} // namespace detail
+
+template <typename in1_t, typename in2_t, typename out_t>
+inline constexpr void atan2(in1_t &&in1, in2_t &&in2, out_t &&out,
+                            const MPMode mpmode = MPMode::NONE) noexcept {
+    const auto rin1 = core::to_mdspan(std::forward<in1_t>(in1));
+    const auto rin2 = core::to_mdspan(std::forward<in2_t>(in2));
+    const auto rout = core::to_mdspan(std::forward<out_t>(out));
+
+    constexpr auto urin1_exts = extents<typename decltype(rin1)::index_type>{};
+    constexpr auto urin2_exts = extents<typename decltype(rin2)::index_type>{};
+    constexpr auto urout_exts = extents<typename decltype(rout)::index_type>{};
+
+    core::batch([](const auto &in1, const auto &in2,
+                   const auto &out) { detail::atan2_impl(in1, in2, out); },
+                std::tuple{rin1, rin2, rout},
+                std::tuple{urin1_exts, urin2_exts, urout_exts}, std::tuple{},
+                mpmode);
+}
+
+template <typename in1_t, typename in2_t>
+[[nodiscard]] inline constexpr auto
+atan2(in1_t &&in1, in2_t &&in2, const MPMode mpmode = MPMode::NONE) noexcept {
+    const auto rin1 = core::to_mdspan(std::forward<in1_t>(in1));
+    const auto rin2 = core::to_mdspan(std::forward<in2_t>(in2));
+
+    constexpr auto urin1_exts = extents<typename decltype(rin1)::index_type>{};
+    constexpr auto urin2_exts = extents<typename decltype(rin2)::index_type>{};
+    constexpr auto urout_exts =
+        extents<std::common_type_t<typename decltype(rin1)::index_type,
+                                   typename decltype(rin2)::index_type>>{};
+
+    return core::batch(
+        [](const auto &in1, const auto &in2, const auto &out) {
+            detail::atan2_impl(in1, in2, out);
+        },
+        std::tuple{rin1, rin2}, std::tuple{urin1_exts, urin2_exts, urout_exts},
+        std::tuple{}, mpmode);
+}
+
+} // namespace ctmd
---
+++ ctmd/ctmd_cos.hpp
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <cmath>
+
+#include "core/ctmd_core.hpp"
+
+namespace ctmd {
+namespace detail {
+
+template <mdspan_c in_t, mdspan_c out_t>
+    requires(in_t::rank() == 0 && out_t::rank() == 0)
+inline constexpr void cos_impl(const in_t &in, const out_t &out) noexcept {
+    out() = std::cos(in());
+}
+
+} // namespace detail
+
+template <typename in_t, typename out_t>
+inline constexpr void cos(in_t &&in, out_t &&out,
+                          const MPMode mpmode = MPMode::NONE) noexcept {
+    const auto rin = core::to_mdspan(std::forward<in_t>(in));
+    const auto rout = core::to_mdspan(std::forward<out_t>(out));
+
+    constexpr auto urin_exts = extents<typename decltype(rin)::index_type>{};
+    constexpr auto urout_exts = extents<typename decltype(rout)::index_type>{};
+
+    core::batch(
+        [](const auto &in, const auto &out) { detail::cos_impl(in, out); },
+        std::tuple{rin, rout}, std::tuple{urin_exts, urout_exts}, std::tuple{},
+        mpmode);
+}
+
+template <typename in_t>
+[[nodiscard]] inline constexpr auto
+cos(in_t &&in, const MPMode mpmode = MPMode::NONE) noexcept {
+    const auto rin = core::to_mdspan(std::forward<in_t>(in));
+
+    constexpr auto urin_exts = extents<typename decltype(rin)::index_type>{};
+    constexpr auto urout_exts = extents<typename decltype(rin)::index_type>{};
+
+    return core::batch(
+        [](const auto &in, const auto &out) { detail::cos_impl(in, out); },
+        std::tuple{rin}, std::tuple{urin_exts, urout_exts}, std::tuple{},
+        mpmode);
+}
+
+} // namespace ctmd
