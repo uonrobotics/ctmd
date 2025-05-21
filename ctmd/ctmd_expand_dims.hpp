782dacb30e5f51dce18c897f8457dae5d0cfd86e
cslim <chansoon0623@gmail.com>
2025-05-21 23:36:53 +0900

    developed

--- ctmd/ctmd_expand_dims.hpp
+++ ctmd/ctmd_expand_dims.hpp
@@ -1,20 +1,20 @@
 #pragma once
 
-#include "core/ctmd_core.hpp"
+#include "ctmd_reshape.hpp"
 
 namespace ctmd {
 
-template <int64_t Axis, typename in_t>
-[[nodiscard]] inline constexpr auto expand_dims(in_t &&in) noexcept {
-    const auto rin = core::to_mdspan(std::forward<in_t>(in));
-    using rin_t = decltype(rin);
+template <int64_t Axis, typename InType>
+[[nodiscard]] inline constexpr auto expand_dims(InType &&In) noexcept {
+    const auto in = core::to_mdspan(std::forward<InType>(In));
+    using in_t = decltype(in);
 
-    constexpr size_t rank = rin_t::rank();
+    constexpr size_t rank = in_t::rank();
 
     if constexpr (rank == 0) {
-        auto new_extents = extents<typename rin_t::index_type, 1>{1};
-        return mdspan<typename rin_t::element_type, decltype(new_extents)>{
-            rin.data_handle(), new_extents};
+        auto new_extents = extents<typename in_t::index_type, 1>{1};
+        return mdspan<typename in_t::element_type, decltype(new_extents)>{
+            in.data_handle(), new_extents};
 
     } else {
         constexpr size_t axis = static_cast<size_t>(
@@ -22,28 +22,17 @@ template <int64_t Axis, typename in_t>
             (rank + 1));
 
         const auto new_extents =
-            [&rin]<size_t... Is>(std::index_sequence<Is...>) {
+            [&in]<size_t... Is>(std::index_sequence<Is...>) {
                 return extents<
-                    typename rin_t::index_type,
+                    typename in_t::index_type,
                     (Is < axis
-                         ? rin_t::static_extent(Is)
-                         : (Is == axis ? 1 : rin_t::static_extent(Is - 1)))...>{
-                    (Is < axis ? rin.extent(Is)
-                               : (Is == axis ? 1 : rin.extent(Is - 1)))...};
+                         ? in_t::static_extent(Is)
+                         : (Is == axis ? 1 : in_t::static_extent(Is - 1)))...>{
+                    (Is < axis ? in.extent(Is)
+                               : (Is == axis ? 1 : in.extent(Is - 1)))...};
             }(std::make_index_sequence<rank + 1>{});
 
-        const auto new_strides =
-            [&rin]<size_t... Is>(std::index_sequence<Is...>) {
-                return std::array<typename rin_t::size_type, rank + 1>{
-                    (Is < axis ? rin.stride(Is)
-                               : (Is == axis ? 1 : rin.stride(Is - 1)))...};
-            }(std::make_index_sequence<rank + 1>{});
-
-        return mdspan<typename rin_t::element_type,
-                      std::remove_const_t<decltype(new_extents)>, layout_stride,
-                      typename rin_t::accessor_type>{
-            rin.data_handle(),
-            layout_stride::mapping{new_extents, new_strides}};
+        return reshape(std::forward<InType>(In), new_extents);
     }
 }
 
