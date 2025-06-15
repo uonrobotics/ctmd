e392d901134be35cb9aefba3445a627c604b9cb3
cslim <chansoon0623@gmail.com>
2025-06-15 22:32:17 +0900

    modified

--- ctmd/core/submdspan.hpp
+++ ctmd/core/submdspan.hpp
@@ -6,36 +6,30 @@
 namespace ctmd {
 namespace core {
 
-template <typename InType, typename... slices_t>
+template <mdspan_c in_t, typename... slices_t>
 [[nodiscard]] inline constexpr auto
-submdspan_from_left(InType &&In, slices_t &&...slices) noexcept {
-    // NOTE: use non-const rin to avoid unnecessary copies
-    auto in = to_mdspan(std::forward<InType>(In));
-
-    static_assert(decltype(in)::rank() >= sizeof...(slices_t),
+submdspan_from_left(const in_t &in, slices_t &&...slices) noexcept {
+    static_assert(in_t::rank() >= sizeof...(slices_t),
                   "The number of slices must not exceed the rank of the input "
                   "mdspan.");
 
     return [&in, &slices...]<size_t... Is>(std::index_sequence<Is...>) {
         return ctmd::submdspan(in, std::forward<slices_t>(slices)...,
                                ((void)Is, ctmd::full_extent)...);
-    }(std::make_index_sequence<decltype(in)::rank() - sizeof...(slices_t)>{});
+    }(std::make_index_sequence<in_t::rank() - sizeof...(slices_t)>{});
 }
 
-template <typename InType, typename... slices_t>
+template <mdspan_c in_t, typename... slices_t>
 [[nodiscard]] inline constexpr auto
-submdspan_from_right(InType &&In, slices_t &&...slices) noexcept {
-    // NOTE: use non-const rin to avoid unnecessary copies
-    auto in = to_mdspan(std::forward<InType>(In));
-
-    static_assert(decltype(in)::rank() >= sizeof...(slices_t),
+submdspan_from_right(const in_t &in, slices_t &&...slices) noexcept {
+    static_assert(in_t::rank() >= sizeof...(slices_t),
                   "The number of slices must not exceed the rank of the input "
                   "mdspan.");
 
     return [&in, &slices...]<size_t... Is>(std::index_sequence<Is...>) {
         return ctmd::submdspan(in, ((void)Is, ctmd::full_extent)...,
                                std::forward<slices_t>(slices)...);
-    }(std::make_index_sequence<decltype(in)::rank() - sizeof...(slices_t)>{});
+    }(std::make_index_sequence<in_t::rank() - sizeof...(slices_t)>{});
 }
 
 } // namespace core
