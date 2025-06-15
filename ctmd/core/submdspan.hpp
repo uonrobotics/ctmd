18c1bd5052267cbd95129f008d61e32b8ed029da
cslim <chansoon0623@gmail.com>
2025-06-16 01:43:23 +0900

    updated

--- ctmd/core/submdspan.hpp
+++ ctmd/core/submdspan.hpp
@@ -6,30 +6,43 @@
 namespace ctmd {
 namespace core {
 
-template <mdspan_c in_t, typename... slices_t>
+template <size_t lspace, size_t rspace, mdspan_c in_t, typename... slices_t>
 [[nodiscard]] inline constexpr auto
-submdspan_from_left(const in_t &in, slices_t &&...slices) noexcept {
-    static_assert(in_t::rank() >= sizeof...(slices_t),
-                  "The number of slices must not exceed the rank of the input "
+submdspan_with_space(const in_t &in, slices_t &&...slices) noexcept {
+    static_assert(in_t::rank() == lspace + sizeof...(slices_t) + rspace,
+                  "The number of slices must match the rank of the input "
                   "mdspan.");
 
-    return [&in, &slices...]<size_t... Is>(std::index_sequence<Is...>) {
-        return ctmd::submdspan(in, std::forward<slices_t>(slices)...,
-                               ((void)Is, ctmd::full_extent)...);
-    }(std::make_index_sequence<in_t::rank() - sizeof...(slices_t)>{});
+    return [&]<size_t... Is, size_t... Js>(std::index_sequence<Is...>,
+                                           std::index_sequence<Js...>) {
+        return ctmd::submdspan(in, ((void)Is, ctmd::full_extent)...,
+                               std::forward<slices_t>(slices)...,
+                               ((void)Js, ctmd::full_extent)...);
+    }(std::make_index_sequence<lspace>{}, std::make_index_sequence<rspace>{});
 }
 
-template <mdspan_c in_t, typename... slices_t>
+template <size_t lspace = 0, mdspan_c in_t, typename... slices_t>
+[[nodiscard]] inline constexpr auto
+submdspan_from_left(const in_t &in, slices_t &&...slices) noexcept {
+    static_assert(
+        in_t::rank() >= lspace + sizeof...(slices_t),
+        "The number of slices must not exceed the rank of the input mdspan.");
+
+    constexpr size_t rspace = in_t::rank() - (lspace + sizeof...(slices_t));
+    return submdspan_with_space<lspace, rspace>(
+        in, std::forward<slices_t>(slices)...);
+}
+
+template <size_t rspace = 0, mdspan_c in_t, typename... slices_t>
 [[nodiscard]] inline constexpr auto
 submdspan_from_right(const in_t &in, slices_t &&...slices) noexcept {
-    static_assert(in_t::rank() >= sizeof...(slices_t),
-                  "The number of slices must not exceed the rank of the input "
-                  "mdspan.");
+    static_assert(
+        in_t::rank() >= rspace + sizeof...(slices_t),
+        "The number of slices must not exceed the rank of the input mdspan.");
 
-    return [&in, &slices...]<size_t... Is>(std::index_sequence<Is...>) {
-        return ctmd::submdspan(in, ((void)Is, ctmd::full_extent)...,
-                               std::forward<slices_t>(slices)...);
-    }(std::make_index_sequence<in_t::rank() - sizeof...(slices_t)>{});
+    constexpr size_t lspace = in_t::rank() - (rspace + sizeof...(slices_t));
+    return submdspan_with_space<lspace, rspace>(
+        in, std::forward<slices_t>(slices)...);
 }
 
 } // namespace core
