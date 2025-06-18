d6f98cc6e43d70902d9bdafea2b6f660118116bf
cslim <chansoon0623@gmail.com>
2025-06-18 23:47:20 +0900

    implement random::uniform

--- ctmd/random/random.hpp
+++ ctmd/random/random.hpp
@@ -1,3 +1,4 @@
 #pragma once
 
 #include "rand.hpp"
+#include "uniform.hpp"
---
+++ tests/random/uniform/main.cpp
@@ -0,0 +1,49 @@
+#include <gtest/gtest.h>
+
+#include "ctmd/array_equiv.hpp"
+#include "ctmd/random/uniform.hpp"
+#include "ctmd/to_string.hpp"
+
+namespace md = ctmd;
+
+TEST(stack, 1) {
+    using T = double;
+
+    constexpr auto out =
+        md::random::uniform<T>(md::extents<size_t, 2, 2>{}, -1, 1);
+
+    std::cout << md::to_string(out) << std::endl;
+
+    ASSERT_TRUE(!md::array_equiv(out, 0));
+}
+
+TEST(stack, 2) {
+    using T = double;
+
+    constexpr auto out =
+        md::random::uniform<T>(md::extents<size_t, 1>{}, -1, 1);
+
+    std::cout << md::to_string(out) << std::endl;
+
+    ASSERT_TRUE(!md::array_equiv(out, 0));
+}
+
+TEST(heap, 1) {
+    using T = double;
+
+    const auto out = md::random::uniform<T>(md::dims<2>{2, 2}, -1, 1);
+
+    std::cout << md::to_string(out) << std::endl;
+
+    ASSERT_TRUE(!md::array_equiv(out, 0));
+}
+
+TEST(heap, 2) {
+    using T = double;
+
+    const auto out = md::random::uniform<T>(md::dims<1>{1}, -1, 1);
+
+    std::cout << md::to_string(out) << std::endl;
+
+    ASSERT_TRUE(!md::array_equiv(out, 0));
+}
