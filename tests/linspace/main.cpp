a2d30442baa0cc9e968aa3301e1c3994a897ae17
cslim <chansoon0623@gmail.com>
2025-06-20 10:30:59 +0900

    update linspace test code

--- tests/linspace/main.cpp
+++ tests/linspace/main.cpp
@@ -1,61 +1,63 @@
 #include <gtest/gtest.h>
 
-#include "ctmd/full.hpp"
+#include "ctmd/array_equal.hpp"
 #include "ctmd/linspace.hpp"
-#include "ctmd/to_string.hpp"
+#include "ctmd/random/uniform.hpp"
 
 namespace md = ctmd;
 
 TEST(stack, 1) {
     using T = double;
 
-    constexpr T start = 1;
-    constexpr T stop = 10;
+    static_assert(md::array_equal(
+        md::linspace((T)2, (T)3, md::extents<uint8_t, 5>{}, true),
+        md::mdarray<T, md::extents<size_t, 5>>{
+            std::array<T, 5>{2, 2.25, 2.5, 2.75, 3}}));
 
-    constexpr size_t num = 10;
-
-    constexpr auto out =
-        md::linspace<0>(start, stop, md::extents<uint8_t, num>{}, true);
-
-    static_assert(md::same(out.extents(), md::extents<size_t, num>{}));
-    static_assert(out(0) == start);
-    static_assert(out(num - 1) == stop);
+    static_assert(md::array_equal(
+        md::linspace((T)2, (T)3, md::extents<uint8_t, 5>{}, false),
+        md::mdarray<T, md::extents<size_t, 5>>{
+            std::array<T, 5>{2, 2.2, 2.4, 2.6, 2.8}}));
 }
 
 TEST(stack, 2) {
     using T = double;
-
-    constexpr T start_val = 1;
-    constexpr T stop_val = 10;
 
     constexpr size_t ext1 = 2;
     constexpr size_t ext2 = 3;
     constexpr size_t num = 10;
 
     constexpr auto start =
-        md::full<T>(start_val, md::extents<size_t, ext1, ext2>{});
+        md::random::uniform<T>(md::extents<size_t, ext1, ext2>{}, 0, 1);
     constexpr auto stop =
-        md::full<T>(stop_val, md::extents<size_t, ext1, ext2>{});
+        md::random::uniform<T>(md::extents<size_t, ext1, ext2>{}, 2, 3);
 
     constexpr auto out1 =
         md::linspace<0>(start, stop, md::extents<uint8_t, num>{}, true);
     constexpr auto out2 =
         md::linspace<1>(start, stop, md::extents<uint8_t, num>{}, true);
     constexpr auto out3 =
+        md::linspace<2>(start, stop, md::extents<uint8_t, num>{}, true);
+    constexpr auto out4 =
         md::linspace<-1>(start, stop, md::extents<uint8_t, num>{}, true);
 
     static_assert(
         md::same(out1.extents(), md::extents<size_t, num, ext1, ext2>{}));
-    static_assert(out1(0, 0, 0) == start_val);
-    static_assert(out1(num - 1, ext1 - 1, ext2 - 1) == stop_val);
+    static_assert(out1(0, 0, 0) == start(0, 0));
+    static_assert(out1(num - 1, ext1 - 1, ext2 - 1) ==
+                  stop(ext1 - 1, ext2 - 1));
 
     static_assert(
         md::same(out2.extents(), md::extents<size_t, ext1, num, ext2>{}));
-    static_assert(out2(0, 0, 0) == start_val);
-    static_assert(out2(ext1 - 1, num - 1, ext2 - 1) == stop_val);
+    static_assert(out2(0, 0, 0) == start(0, 0));
+    static_assert(out2(ext1 - 1, num - 1, ext2 - 1) ==
+                  stop(ext1 - 1, ext2 - 1));
 
     static_assert(
         md::same(out3.extents(), md::extents<size_t, ext1, ext2, num>{}));
-    static_assert(out3(0, 0, 0) == start_val);
-    static_assert(out3(ext1 - 1, ext2 - 1, num - 1) == stop_val);
+    static_assert(out3(0, 0, 0) == start(0, 0));
+    static_assert(out3(ext1 - 1, ext2 - 1, num - 1) ==
+                  stop(ext1 - 1, ext2 - 1));
+
+    static_assert(md::array_equal(out3, out4));
 }
