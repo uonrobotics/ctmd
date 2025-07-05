5dae04769fd0027823839edcffd0fdb36752d7b9
cslim <chansoon0623@gmail.com>
2025-07-05 17:57:35 +0900

    update empty_like

--- ctmd/empty_like.hpp
+++ ctmd/empty_like.hpp
@@ -11,4 +11,10 @@ template <typename InType>
     return ctmd::empty<T>(in.extents());
 }
 
+template <typename T, typename InType>
+[[nodiscard]] inline constexpr auto empty_like(InType &&In) noexcept {
+    const auto in = core::to_const_mdspan(std::forward<InType>(In));
+    return ctmd::empty<T>(in.extents());
+}
+
 } // namespace ctmd
