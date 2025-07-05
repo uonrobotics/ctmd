190590f593b8d5f377fd8e412be1b25212111e80
cslim <chansoon0623@gmail.com>
2025-07-05 17:55:23 +0900

    change function name

--- ctmd/absolute.hpp
+++ ctmd/absolute.hpp
@@ -7,7 +7,7 @@ namespace detail {
 
 template <mdspan_c in_t, mdspan_c out_t>
     requires(in_t::rank() == 0 && out_t::rank() == 0)
-inline constexpr void abs_impl(const in_t &in, const out_t &out) noexcept {
+inline constexpr void absolute_impl(const in_t &in, const out_t &out) noexcept {
 #ifdef REAL_GCC
     out() = std::abs(in());
 
@@ -21,11 +21,11 @@ inline constexpr void abs_impl(const in_t &in, const out_t &out) noexcept {
 } // namespace detail
 
 template <typename InType, typename OutType>
-inline constexpr void abs(InType &&In, OutType &&Out,
-                          const MPMode mpmode = MPMode::NONE) noexcept {
+inline constexpr void absolute(InType &&In, OutType &&Out,
+                               const MPMode mpmode = MPMode::NONE) noexcept {
     core::batch(
         [](auto &&...elems) {
-            detail::abs_impl(std::forward<decltype(elems)>(elems)...);
+            detail::absolute_impl(std::forward<decltype(elems)>(elems)...);
         },
         std::index_sequence<0, 0>{}, mpmode,
         core::to_const_mdspan(std::forward<InType>(In)),
@@ -34,10 +34,10 @@ inline constexpr void abs(InType &&In, OutType &&Out,
 
 template <typename InType>
 [[nodiscard]] inline constexpr auto
-abs(InType &&In, const MPMode mpmode = MPMode::NONE) noexcept {
+absolute(InType &&In, const MPMode mpmode = MPMode::NONE) noexcept {
     return core::batch_out(
         [](auto &&...elems) {
-            detail::abs_impl(std::forward<decltype(elems)>(elems)...);
+            detail::absolute_impl(std::forward<decltype(elems)>(elems)...);
         },
         std::index_sequence<0>{}, ctmd::extents<uint8_t>{}, mpmode,
         core::to_const_mdspan(std::forward<InType>(In)));
