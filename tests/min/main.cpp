beef460c0f89af02811b9d2c3795a595d1544045
cslim <chansoon0623@gmail.com>
2025-08-05 13:38:05 +0900

    implement min

---
+++ tests/min/main.cpp
@@ -0,0 +1,25 @@
+#include <gtest/gtest.h>
+
+#include "ctmd/ctmd_array_equal.hpp"
+#include "ctmd/ctmd_min.hpp"
+
+namespace md = ctmd;
+
+TEST(test, 1) {
+    using T = int;
+
+    static_assert(
+        md::array_equal(md::min(md::mdarray<T, md::extents<uint8_t, 2, 2>>{
+                            std::array<T, 4>{0, 1, 2, 3}}),
+                        0));
+
+    static_assert(md::array_equal(
+        md::min<0>(md::mdarray<T, md::extents<uint8_t, 2, 2>>{
+            std::array<T, 4>{0, 1, 2, 3}}),
+        md::mdarray<T, md::extents<uint8_t, 2>>{std::array<T, 2>{0, 1}}));
+
+    static_assert(md::array_equal(
+        md::min<1>(md::mdarray<T, md::extents<uint8_t, 2, 2>>{
+            std::array<T, 4>{0, 1, 2, 3}}),
+        md::mdarray<T, md::extents<uint8_t, 2>>{std::array<T, 2>{0, 2}}));
+}
