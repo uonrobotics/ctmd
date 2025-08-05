605ce0d24a3b65578596baa1b4213a8806641a83
cslim <chansoon0623@gmail.com>
2025-08-05 15:35:02 +0900

    solve bug

--- ctmd/creation/ctmd_ones.hpp
+++ ctmd/creation/ctmd_ones.hpp
@@ -6,9 +6,9 @@ namespace ctmd {
 
 template <typename dtype, extents_c exts_t = extents<size_t>>
 [[nodiscard]] inline constexpr auto
-zeros(const exts_t &exts = exts_t{},
-      const ctmd::MPMode mpmode = ctmd::MPMode::NONE) noexcept {
-    return ctmd::full<dtype>(0, exts, mpmode);
+ones(const exts_t &exts = exts_t{},
+     const ctmd::MPMode mpmode = ctmd::MPMode::NONE) noexcept {
+    return ctmd::full<dtype>(1, exts, mpmode);
 }
 
 } // namespace ctmd
